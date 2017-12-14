4f1a73e97043a8a6b0bf3926e2d5d83cbfa43120
Anton Ledström <anton.ledstrom@digitalent.se>
2017-12-14 15:28:53 +0100

    completed step 4, added views etc

--- App.js
+++ App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import { Provider } from 'react-redux'; 
-import { StyleSheet, Text, View } from 'react-native';
-import store from './store'; 
+import { Provider } from 'react-redux';
+import { StackNavigator } from 'react-navigation';
+import store from './store';
+import MainScreen from './screens/MainScreen'; 
+
 
 export default class App extends React.Component {
   render() {
+    const MainNavigator = StackNavigator({
+      Main: {
+        screen: MainScreen
+      }
+    }); 
+
     return (
-      <Provider store={store}> 
-        <View style={styles.container}>
-          <Text>App.js is now using a Store! Open up App.js to start working on your app!</Text>
-        </View>
+      <Provider store={store} >
+        <MainNavigator />
       </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
